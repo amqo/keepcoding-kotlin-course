6a97f35d1f88a37e173c0d36c93a568e5db930db
ALTRAN_ES\alberto.quiros <alberto.munoz@altran.com>
2018-01-24 19:01:11 +0100

    Replace fragments in fragment container when item selected.

--- app/src/main/java/com/kotlin/albertoquiros/gangame/MainActivity.kt
+++ app/src/main/java/com/kotlin/albertoquiros/gangame/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.kotlin.albertoquiros.gangame
 
 import android.os.Bundle
+import android.support.v4.app.Fragment
 import android.support.v7.app.AppCompatActivity
+import com.kotlin.albertoquiros.gangame.deals.DealsFragment
+import com.kotlin.albertoquiros.gangame.owned.TopOwnedFragment
+import com.kotlin.albertoquiros.gangame.rated.TopRatedFragment
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -10,11 +14,40 @@ class MainActivity : AppCompatActivity() {
         const val defaultOption = R.id.action_deals
     }
 
+    val fragments: HashMap<Int, Fragment> = hashMapOf(
+            Pair(R.id.action_deals, DealsFragment()),
+            Pair(R.id.action_top_rated, TopRatedFragment()),
+            Pair(R.id.action_most_owned, TopOwnedFragment())
+    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        initView()
+
         navigationView.selectedItemId = defaultOption
+        navigationView.setOnNavigationItemReselectedListener { item ->
+            val fragment = fragments[item.itemId]
+            fragment?.let {
+                replaceFragment(fragment)
+            }
+            true
+        }
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+                .replace(R.id.fragmentContainer, fragment)
+                .commit()
+    }
+
+    private fun initView() {
+        val currentFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainer)
+        if (currentFragment == null) {
+            supportFragmentManager.beginTransaction()
+                    .add(R.id.fragmentContainer, fragments[defaultOption])
+                    .commit()
+        }
     }
 }
