688cf705a5dac5a29ffbe80897f9aad741187d3d
ALTRAN_ES\alberto.quiros <alberto.munoz@altran.com>
2018-03-04 11:56:45 +0100

    Create BaseListFragment in commons library.

---
+++ commons/src/main/java/com/amqo/commons/BaseListFragment.kt
@@ -0,0 +1,30 @@
+package com.amqo.commons
+
+import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import android.view.View
+import kotlinx.android.synthetic.main.fragment_base_list.view.*
+
+abstract class BaseListFragment : BaseFragment() {
+
+    val listAdapter: RecyclerView.Adapter<*>
+        get() = getAdapter()
+
+    override fun getLayoutRes(): Int {
+        return R.layout.fragment_base_list
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        view?.list?.let {
+            with(view.list) {
+                adapter = listAdapter
+                layoutManager = LinearLayoutManager(context)
+            }
+        }
+    }
+
+    abstract fun getAdapter() : RecyclerView.Adapter<*>
+}
