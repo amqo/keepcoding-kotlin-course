08e8cd31cb45cf40d9d8daf64ea03207e24ddab5
ALTRAN_ES\alberto.quiros <alberto.munoz@altran.com>
2018-03-25 16:38:06 +0200

    Unit test deals response.

--- sdk/src/testMock/java/com/amqo/sdk/RequestTest.kt
+++ sdk/src/testMock/java/com/amqo/sdk/RequestTest.kt
@@ -1,5 +1,6 @@
 package com.amqo.sdk
 
+import com.google.gson.JsonParser
 import junit.framework.Assert
 import org.junit.Test
 
@@ -11,10 +12,24 @@ class RequestTest {
         val response = apiService.apiClient.getDeals().execute()
         val deals = response.body()
 
+        val parser = JsonParser()
+        val jsonResponse = parser.parse(MockResponses.DEALS_RESPONSE).asJsonArray
+
         Assert.assertTrue(response.isSuccessful)
 
         deals?.let {
-            Assert.assertEquals(deals.size, 4)
+            Assert.assertEquals(deals.size, jsonResponse.size())
+
+            deals.zip(jsonResponse).forEach {(deal,  jsonDeal) ->
+                with(jsonDeal.asJsonObject) {
+                    Assert.assertEquals(deal.title, this["title"].asString)
+                    Assert.assertEquals(deal.salePrice, this["salePrice"].asFloat)
+                    Assert.assertEquals(deal.normalPrice, this["normalPrice"].asFloat)
+                    Assert.assertEquals(deal.metacriticScore, this["metacriticScore"].asInt)
+                    Assert.assertEquals(deal.steamRating, this["steamRatingPercent"].asInt)
+                    Assert.assertEquals(deal.thumb, this["thumb"].asString)
+                }
+            }
         }
     }
 }
